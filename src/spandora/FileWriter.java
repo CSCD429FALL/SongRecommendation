8ebb2ad61639e1b9f8aeb89af0092dcea069de15
Sergio Ramirez <ramirser000@gmail.com>
2016-11-26 23:11:14 -0800

    Raw Gui and Accuracy Testing

--- src/spandora/FileWriter.java
+++ src/spandora/FileWriter.java
@@ -1,35 +1,30 @@
 package spandora;
 
-
 import java.io.PrintWriter;
 
 public class FileWriter {
-	
-	public static void write(String filePath, String[][] data){
-		
-		try{
+
+	public static void write(String filePath, String[][] data) {
+
+		try {
 			PrintWriter pw = new PrintWriter(filePath);
-			
-			for(int row = 0; row < data.length; row++){
-				for(int col = 0; col < data[row].length; col++){
-					if(col < data[row].length - 1){						
+
+			for (int row = 0; row < data.length; row++) {
+				for (int col = 0; col < data[row].length; col++) {
+					if (col < data[row].length - 1) {
 						pw.print(data[row][col] + ",");
-					}
-					else{
+					} else {
 						pw.print(data[row][col]);
 					}
 				}
 				pw.println();
 			}
-			
+
 			pw.close();
-			
-			
-		}
-		catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 }
